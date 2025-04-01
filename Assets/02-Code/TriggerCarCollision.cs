548fd0ba36a828a21828bf59e9235a199fe3549b
Luis Zuniga <luiszu7779@gmail.com>
2025-04-01 10:13:26 +0200

    Meilleure gestion des collisions poulet-voiture

--- Assets/02-Code/TriggerCarCollision.cs
+++ Assets/02-Code/TriggerCarCollision.cs
@@ -6,20 +6,28 @@ public class TriggerCarCollision : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.name != "Poulet")
-        {
+        if (other.name != "Poulet") return;
+        Debug.Log("Collision avec Poulet");
+
+        LaunchPouletInSpace(other);
+        // Event mort du poulet
+    }
+
+    private void LaunchPouletInSpace(Collider poulet)
+    {
+        Rigidbody rb = poulet.GetComponent<Rigidbody>();
+        if (rb == null) {
             return;
         }
 
-        Rigidbody rb = other.GetComponent<Rigidbody>();
-        if (rb != null)
-        {
-            // Appliquer une force vers le haut et en arrière pour le faire voler en vrille
-            Vector3 launchDirection = (Vector3.up * launchForce) + (Vector3.forward * Random.Range(-5f, 5f)) + (Vector3.right * Random.Range(-5f, 5f));
+        // Appliquer une force vers le haut et en arrière pour le faire voler en vrille
+        Vector3 launchDirection = (Vector3.up * launchForce) + (Vector3.forward * UnityEngine.Random.Range(-15f, 15f)) + (Vector3.right * UnityEngine.Random.Range(-15f, 15f));
+        rb.AddForce(launchDirection, ForceMode.Impulse);
+        rb.AddForce(launchDirection, ForceMode.VelocityChange);
+        rb.AddForce(launchDirection, ForceMode.Acceleration);
+        rb.AddForce(launchDirection, ForceMode.Force);
 
-            rb.AddForce(launchDirection, ForceMode.Impulse);
-            // Ajoute une rotation aléatoire
-            rb.AddTorque(Random.insideUnitSphere * launchForce * 2, ForceMode.Impulse);
-        }
+        // Ajoute une rotation aléatoire
+        rb.AddTorque(UnityEngine.Random.insideUnitSphere * launchForce * 2, ForceMode.Impulse);
     }
 }
